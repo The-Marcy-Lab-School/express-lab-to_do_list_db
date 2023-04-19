dece57149ccd3792129434a1e86eb570db5babf4
benspector-mls <ben@marcylabschool.org>
2023-04-19 09:45:40 -0400

    Revert "9 tests passing"
    This reverts commit fa98ff54c21704f85f1b87012d3d1eb8271ca4b7.

--- src/db/models/to-do.js
+++ src/db/models/to-do.js
@@ -1,75 +1,15 @@
 const knex = require('./knex');
-const tableName = 'to_dos'
 
 class ToDo {
-  static async create(text) {
-    try {
-      const result = await knex.raw(`
-        INSERT INTO ${tableName} (title, is_done)
-        VALUES (?, false)
-        RETURNING *;
-      `, [text]);
-      return result.rows[0];
-    } catch (err) {
-      console.error(err);
-      return null;
-    }
-  }
+  static async create() {}
 
-  static async list() {
-    try {
-      const result = await knex.raw(`
-        SELECT *
-        FROM ${tableName};
-      `);
-      return result.rows;
-    } catch (err) {
-      console.error(err);
-      return null;
-    }
-  }
+  static async list() {}
 
-  static async find(id) {
-    try {
-      const result = await knex.raw(`
-        SELECT *
-        FROM ${tableName}
-        WHERE id=?;
-      `, id);
-      return result.rows[0];
-    } catch (err) {
-      console.error(err);
-      return null;
-    }
-  }
+  static async find() {}
 
-  static async updateCompletion(id, isDone) {
-    try {
-      const result = await knex.raw(`
-        UPDATE ${tableName}
-        SET is_done=?
-        WHERE id=?
-        RETURNING *;
-      `, [isDone, id]);
-      return result.rows[0];
-    } catch (err) {
-      console.error(err);
-      return null;
-    }
-  }
+  static async updateCompletion() {}
 
-  static async delete(id) {
-    try {
-      const result = await knex.raw(`
-        DELETE FROM ${tableName}
-        WHERE id=?;
-      `, id);
-      return result.rowCount ? true : false;
-    } catch (err) {
-      console.error(err);
-      return null;
-    }
-  }
+  static async delete() {}
 
   static async deleteAll() {
     try {
